f6ddebb6c8339354659eef0f68b51311ad75849f
rakha fadilah <raka.fadhilah123@gmail.com>
2025-04-08 19:24:11 +0700

    add .env file

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,6 +7,13 @@ chown -R www-data:www-data /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage
 chmod -R 775 /var/www/bootstrap/cache
 
+if grep -q "^APP_KEY=$" /var/www/.env; then
+  echo "APP_KEY kosong, menjalankan php artisan key:generate..."
+  php artisan key:generate
+else
+  echo "APP_KEY sudah terisi, skip php artisan key:generate"
+fi
+
 php artisan migrate
 
 php-fpm
